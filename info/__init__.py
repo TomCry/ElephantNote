e6ab2c62972447556f14cd0c2594721b81328532
TomCry <zycctom@outlook.com>
2020-01-16 00:40:08 -0500

    09-配置名字作为参数

--- info/__init__.py
+++ info/__init__.py
@@ -6,14 +6,19 @@ from redis import StrictRedis
 
 from config import config
 
-app = Flask(__name__)
-# 加载配置
-app.config.from_object(config['dev'])
+# 在flask很多扩展里面都可以先初始化，然后init_app方法初始化
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
+def create_app(config_name):
+    app = Flask(__name__)
+    # 加载配置
+    app.config.from_object(config[config_name])
+    # 通过app初始化
+    db.init_app(app)
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST,port=config[config_name].REDIS_PORT)
+    # 开启csrf保护,只做服务器验证功能
+    CSRFProtect(app)
+    # 设置session保存指定位置
+    Session(app)
 
-redis_store = StrictRedis(host=config['dev'].REDIS_HOST,port=config['dev'].REDIS_PORT)
-# 开启csrf保护,只做服务器验证功能
-CSRFProtect(app)
-# 设置session保存指定位置
-Session(app)
+    return app
