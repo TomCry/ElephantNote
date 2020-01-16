57efca3f76bf48715f3a4b5accb8cd558cbef8cc
TomCry <zycctom@outlook.com>
2020-01-15 20:25:14 -0500

    项目创建及数据库配置

--- manage.py
+++ manage.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+    # 为Mysql添加配置
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:enzyme0313@127.0.0.1:3306/elephant"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
