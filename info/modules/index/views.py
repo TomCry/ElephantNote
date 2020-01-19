0f6a47f2cd787e83f7f2caf2855b7c56c8585df9
TomCry <zycctom@outlook.com>
2020-01-19 11:15:28 -0500

    03-01跟路由的设置

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,12 @@
+from flask import render_template
+
 from info import redis_store
 from . import index_blu
 
 
 @index_blu.route('/')
 def index():
-    # redis保存值
-    redis_store.set("name","notation")
-    return "index"
+    # # redis保存值
+    # redis_store.set("name","notation")
+
+    return render_template('news/index.html')
