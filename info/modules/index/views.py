7667d72836dc2079e670895bc1746fffaa0a2e8c
TomCry <zycctom@outlook.com>
2020-01-19 11:29:57 -0500

    03-02加载favicon图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from info import redis_store
 from . import index_blu
@@ -10,3 +10,9 @@ def index():
     # redis_store.set("name","notation")
 
     return render_template('news/index.html')
+
+
+# 在打开网页的时候，浏览器会请求根路径+favicon.icon小图标
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
