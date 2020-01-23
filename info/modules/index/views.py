87164f538d643e65ccdddc40ebaf33b84a54093c
TomCry <zycctom@outlook.com>
2020-01-23 00:52:45 -0500

    04-07 修改右上角登录状态(csrf)还没修改，目前报错未知

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,7 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 from info import redis_store
+from info.models import User
 from . import index_blu
 
 
@@ -8,8 +9,29 @@ from . import index_blu
 def index():
     # # redis保存值
     # redis_store.set("name","notation")
+    """
+    1. 如果用户已经登录，将当前用户的数据传入模板，进行显示
+    2.
+    :return:
+    """
+    # 1
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
 
-    return render_template('news/index.html')
+
+    data = {
+        "user": user.to_dict() if user else None
+    }
+
+
+
+
+    return render_template('news/index.html', data=data)
 
 
 # 在打开网页的时候，浏览器会请求根路径+favicon.icon小图标
