f45b8a3b0225aa2a64cb490d5754f28a9fb3ce31
TomCry <zycctom@outlook.com>
2020-04-23 16:55:37 -0400

    05-03 首页基类模板抽取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -3,14 +3,16 @@ from flask import render_template
 from info.modules.news import news_blu
 
 
-#127.0.0.1:9999/news/2
-@news_blu('/<int:news_id>')
+# 127.0.0.1:9999/news/2
+@news_blu.route('/<int:news_id>')
 def news_detail(news_id):
-
     """
     新闻详情
     :param news_id:
     :return:
     """
+    data = {
 
-    return render_template("news/detail.html")
+    }
+
+    return render_template("news/detail.html",data=data)
