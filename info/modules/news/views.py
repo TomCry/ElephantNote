96f6eeedae6a319081878e06881d03845921d924
TomCry <zycctom@outlook.com>
2020-04-28 10:15:35 -0400

    05-04 新闻详情基本模板显示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, current_app, session
 
+from info import constants
+from info.models import News, User
 from info.modules.news import news_blu
 
 
@@ -11,8 +13,31 @@ def news_detail(news_id):
     :param news_id:
     :return:
     """
-    data = {
 
+    # 查询用户登录信息
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    news_list = []
+    # 右侧新闻排行逻辑
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_list = []
+    # 遍历对象列表，将对象的字典添加到字典列表中
+    for news in news_list:
+        news_dict_list.append(news.to_basic_dict())
+
+    data = {
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_list
     }
 
     return render_template("news/detail.html",data=data)
