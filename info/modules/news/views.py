b4e7806a989a67872741ad7975fd145f29ae9336
TomCry <zycctom@outlook.com>
2020-04-28 14:10:20 -0400

    05-08 显示是否收藏界面逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -53,11 +53,17 @@ def news_detail(news_id):
 
     # 更新新闻点击次数
     news.clicks += 1
+    is_collected = False
+
+    # 如果用户已登录：
+    # 判断用户是否收藏当前新闻，如果收藏：
+    # is_collected = True
 
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_list,
-        "news": news.to_dict()
+        "news": news.to_dict(),
+        "is_collected": is_collected
     }
 
     return render_template("news/detail.html",data=data)
