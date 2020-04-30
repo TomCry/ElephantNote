3d0cacc2219974504d18c9c63ee0d0f4cd6d597b
TomCry <zycctom@outlook.com>
2020-04-30 18:15:26 -0400

    06-07 基本资料修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, g, redirect, request
+from flask import render_template, g, redirect, request, jsonify
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
 @profile_blu.route('/base_info', methods=["GET", "POST"])
@@ -11,6 +12,23 @@ def base_info():
     if request.method == "GET":
         return render_template('news/user_base_info.html', data={"user": user.to_dict()})
     # 修改用户数据，不同的请求方式做不同的事情
+    # 1.取到传入的参数
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    # 2.校验参数
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if gender not in ("WOMAN", "MAN"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 
