3e41becd3bf63cb45f276507c0491a30e60f24de
TomCry <zycctom@outlook.com>
2020-04-30 17:56:09 -0400

    06-06 基本资料页面展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,17 @@
-from flask import render_template, g, redirect
+from flask import render_template, g, redirect, request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+
+
+@profile_blu.route('/base_info', methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    user = g.user
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user": user.to_dict()})
+    # 修改用户数据，不同的请求方式做不同的事情
+
 
 
 @profile_blu.route('/info')
