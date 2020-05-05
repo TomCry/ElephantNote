7772f6a5572c73d52bc30307dcf393e8e248e46b
TomCry <zycctom@outlook.com>
2020-05-05 12:59:23 -0400

    07-07 创建管理员集成到命令行

--- manage.py
+++ manage.py
@@ -9,6 +9,8 @@ from info import create_app, db, models
 # manage.py是程序启动的入口，只关心启动的相关参数以及内容，不关心具体业务逻辑
 
 # 通过指定的配置名字创建对应配置的app
+from info.models import User
+
 app = create_app('dev')
 
 manager = Manager(app)
@@ -18,6 +20,26 @@ Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.option('-n', '-name', dest="name")
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+    if not all([name, password]):
+        print("参数不足")
+
+    user = User()
+    user.nick_name = name
+    user.mobile = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+
+    print("添加成功")
 
 
 
