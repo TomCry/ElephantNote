92d8680cf72cffd4c08b5f79ad19edd00b44b906
TomCry <zycctom@outlook.com>
2020-05-05 13:32:26 -0400

    07-09 判断管理员是否已登录，如果已登录则重定向到首页

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -10,7 +10,15 @@ def index():
 @admin_blu.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
+        # 判断当前是否有登录，如果有登录直接重定向到管理员后台主页
+        user_id = session.get("user_id", None)
+        is_admin = session.get("is_admin", False)
+
+        if user_id and is_admin:
+            return redirect(url_for("admin.index"))
+
         return render_template('admin/login.html')
+
 
     # 取到登录的参数
     username = request.form.get("username")
